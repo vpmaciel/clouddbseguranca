ae3beb2c100fe864269c2e91052cdf4101fe902a
Vicente <Vicente@PRESARIO-CQ21>
2020-07-14 17:09:49 -0300

    A

--- controller/relatorio.php
+++ controller/relatorio.php
@@ -1,40 +1,40 @@
 <?php
 require_once '../database/sql.php';
-//$values = $pdo->query('exec stpSecurity_Checklist');
+
 //echo gettype($value), "\n";
 //echo $value, "\n";
-
-
-foreach($pdo->query( 'exec stpSecurity_Checklist' ) as $row)
-{
-    print_r($row);
-    echo "<br>";
-}
-
-
-/*
-while ($row = sqlsrv_fetch_array($stmt)or die(print_r(sqlsrv_errors()))) {
-    $coluna = array();
-    $coluna['Id_Verificacao'] = $row['Id_Verificacao'];
-    $coluna['Ds_Categoria'] = $row['Ds_Categoria'];
-    $coluna['Ds_Titulo'] = $row['Ds_Titulo'];
-    $coluna['Ds_Resultado'] = $row['Ds_Resultado'];
-    $coluna['Ds_Descricao'] = $row['Ds_Descricao'];
-    $coluna['Ds_Verificacao'] = $row['Ds_Verificacao'];
-    $coluna['Ds_Sugestao'] = $row['Ds_Sugestao'];
-    $coluna['Ds_Detalhes'] = $row['Ds_Detalhes'];
-    array_push( $lista, $coluna);
-}
-
-$stmt = null;
-echo json_encode($coluna);
-echo "<br>";
-
-if (count($lista) > 0) {
-    print_r($lista);
-    //header('Location: ../view/sucesso.php');
-} else {
-    //header('Location: ../view/erro.php');
-}
-*/
 ?>
+
+<table style="border:1px solid black;" bgcolor="#CCC" width="100%">
+
+<?php
+$contador = 0;
+foreach($pdo->query( 'exec stpSecurity_Checklist' ) as $key => $value)
+{
+    $contador++;
+    if($contador % 2 == 0) {
+        echo('<tr><td style="color:gray;""><b>CÓDIGO:</b> '.$value['Código']); echo "</td></tr>";
+        echo('<tr><td style="color:gray;"><b>CATEGORIA:</b> '.$value['Categoria']); echo "</td></tr>";
+        echo('<tr><td style="color:gray;"><b>O QUE É VERIFICADO:</b> '.$value['O que é verificado']); echo "</td></tr>";
+        echo('<tr><td style="color:gray;"><b>AVALIAÇÃO:</b> '.$value['Avaliação']); echo "</td></tr>";
+        echo('<tr><td style="color:gray;"><b>DESCRIÇÃO DO PROBLEMA:</b> '.$value['Descrição do Problema']); echo "</td></tr>";
+        echo('<tr><td style="color:gray;"><b>DETALHAMENTO DA VERIFICAÇÃO:</b> '.$value['Detalhamento da Verificação']); echo "</td></tr>";
+        echo('<tr><td style="color:gray;"><b>SUGESTÃO DE CORREÇÃO:</b> '.$value['Sugestão de Correção']); echo "</td></tr>";
+        echo('<tr><td style="color:gray;"><b>RESULTADOS DA VALIDAÇÃO:</b> '.$value['Resultados da Validação']); echo "</td></tr>";
+        echo('<tr><td style="color:gray;"><b>URL DE REFERÊNCIA:</b> '.$value['URL de Referência']); echo "</td></tr>";
+    } else {
+        echo('<tr><td style="color:black;"><b>CÓDIGO:</b> '.$value['Código']); echo "</td></tr>";
+        echo('<tr><td style="color:black;"><b>CATEGORIA:</b> '.$value['Categoria']); echo "</td></tr>";
+        echo('<tr><td style="color:black;"><b>O QUE É VERIFICADO:</b> '.$value['O que é verificado']); echo "</td></tr>";
+        echo('<tr><td style="color:black;"><b>AVALIAÇÃO:</b> '.$value['Avaliação']); echo "</td></tr>";
+        echo('<tr><td style="color:black;"><b>DESCRIÇÃO DO PROBLEMA:</b> '.$value['Descrição do Problema']); echo "</td></tr>";
+        echo('<tr><td style="color:black;"><b>DETALHAMENTO DA VERIFICAÇÃO:</b> '.$value['Detalhamento da Verificação']); echo "</td></tr>";
+        echo('<tr><td style="color:black;"><b>SUGESTÃO DE CORREÇÃO:</b> '.$value['Sugestão de Correção']); echo "</td></tr>";
+        echo('<tr><td style="color:black;"><b>RESULTADOS DA VALIDAÇÃO:</b> '.$value['Resultados da Validação']); echo "</td></tr>";
+        echo('<tr><td style="color:black;"><b>URL DE REFERÊNCIA:</b> '.$value['URL de Referência']); echo "</td></tr>";
+        
+    }
+    echo'<tr><td>&nbsp;</td></tr>';
+}
+?>
+</table>
